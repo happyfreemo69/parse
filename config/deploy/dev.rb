528ff5f879c8fbc7aa0b8536263a0f52b38de05b
mpech <maxime.pech@freemo.blue>
2018-03-08 12:05:34 +0100

    #no remove capistrano config

--- config/deploy/dev.rb
+++
@@ -1,6 +0,0 @@
-set :deploy_to, '/home/deployer/parse'
-set :user, 'deployer'
-set :pid_file_name, 'parse.pid'
-set :branch, 'dev'
-set :url_ping, 'https://notif-dev.citylity.com/parse'
-role :app, %w{deployer@185.8.50.133}
--- config/deploy/prd.rb
+++
@@ -1,7 +0,0 @@
-set :deploy_to, '/home/deployer/parse'
-set :user, 'deployer'
-set :pid_file_name, 'deployer.pid'
-set :branch, 'prd'
-set :url_ping, 'https://notif.citylity.com/parse'
-role :app, %w{deployer@89.40.113.104}
-server '89.40.113.104', user: 'deployer', roles: %w{web}
