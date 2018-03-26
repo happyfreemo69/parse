b2f0b028eee7763d27511194c554ec665b824c6a
mpech <maxime.pech@freemo.blue>
2018-03-26 12:07:46 +0200

    [TG-4328] parse on lgs

--- config/index.js
+++ config/index.js
@@ -18,34 +18,37 @@ exports.install_endpoint = '/inner%/install';
 exports.parse_pri_host = '127.0.0.1';
 exports.enable_langInPayload = true;
 exports.loco_notifNs = 'TRADCLINS_';
-exports.hot = {
-    compression_enable:true,
-    sendMails : exports.mode === 'prod',
-    logLvl: 0,
-    logToConsole: false,
-    logMaxFileSize:5000000,
-    logLineMaxSize: exports.mode == 'prod'?4096:4000000,
-    winston_host: 'goto.papertrailapp.com',
-    winston_port: 42,
-    winston_hostname: "NOHOST",
-    winston_slackUrl:'override me, private webhook url',
-    winston_pptOnError:true,
-    winston_enable: false,
-};
 exports.notif_withDisplay_android = '4.1.8';
 exports.notif_withDisplay_ios = '402000002';
 exports.parse = {
     application_id: "Xe2KZ2QFNCgSkFIhXea5nTYz5sjtuYuZ943EXSmT",
     javascript_key: "WuTl7JWodREaHN92YhvvVL7dAQ9jtEKdyqNFQkEk"
 }
+exports.lgs_appName = 'override me in private';
+exports.lgs_pri_host = 'override me in urlDns';
 
+exports.hot = {};
 exports.parse_logUserIds = false;
 
 var fs = require('fs');
 var res = fs.existsSync(__dirname+'/privateConfig.js') && require('./privateConfig.js');
-for(var i in res){
-    exports[i] = res[i];//warn of embedded object
-}
+Object.assign(exports, res);//no deep stuff
+Object.assign(exports, require('./urlDns'));
 
-exports.hot.winston_slackPptDownMessage = exports.phase+' - '+exports.hot.winston_hostname+':papertrails is down (%date%)';
-exports.logger = new Logger({path: __dirname+'/../log/'}, exports.hot);
+var lgsOptions = {};
+lgsOptions.logLvl = 0;
+lgsOptions.logToConsole = false;
+lgsOptions.logMaxFileSize =5000000;
+lgsOptions.logLineMaxSize = 1e4;
+lgsOptions.lgs_socketRecycleTime = 100;
+lgsOptions.lgs_maxRetries = 5;
+lgsOptions.lgs_timeoutBeforeRetry = 2000;
+lgsOptions.lgs_processTimeout = 1000;
+lgsOptions.lgs_maxBlockSize = 5e5;//remote is 1e6
+lgsOptions.lgs_minFullLength = 1e7;
+lgsOptions.lgs_unlockAfter = 5000;//retry 5seconds after if needed
+lgsOptions.lgs_tickEvery = 1000;
+exports.logger = exports.http_logger == 0?
+    new Logger({path: __dirname+'/../log/'}, lgsOptions): exports.http_logger == 1?
+    new (Logger.http)({appName:exports.lgs_appName, host: exports.lgs_pri_host, port:exports.lgs_pri_port}, lgsOptions):
+    new (Logger.http2)({appName:exports.lgs_appName, host: exports.lgs_pri_host, port:exports.lgs_pri_port}, lgsOptions)
---
+++ config/urlDns.js
@@ -0,0 +1,13 @@
+/*
+Purpose of this file
+It overrides config
+ */
+exports = module.exports;
+var config = require('./');
+var Network = require('nodelibs').Network;
+var network = new Network(config.phase);
+
+exports.lgs_pri_port = network.lgs().pri().port();
+exports.lgs_pri_host = network.lgs().pri().host();
+
+exports.admserv_pub_url = network.admserv().pub().url();
